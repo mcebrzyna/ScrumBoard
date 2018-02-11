59adf7fde95755d6acb4aed112a62658b649f9d0
Michal Cebrzyna <mcebrzyna@gmail.com>
2018-02-11 18:06:57 +0100

    loader new style

--- js/other/loader.jsx
+++ js/other/loader.jsx
@@ -5,7 +5,12 @@ class Loader extends React.Component{
     render(){
         return (
             <div className='loader'>
-                <div className="spinner"/>
+                <div className="sk-folding-cube">
+                    <div className="sk-cube1 sk-cube"/>
+                    <div className="sk-cube2 sk-cube"/>
+                    <div className="sk-cube4 sk-cube"/>
+                    <div className="sk-cube3 sk-cube"/>
+                </div>
             </div>
         )
     }
